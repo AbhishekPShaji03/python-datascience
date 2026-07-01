d5060a513278f6c12afd18d4ab0ae30de79e718d
Abhishek P Shaji <abhishekpshaji3@gmail.com>
2026-07-01 13:01:53 +0530

    Add files via upload

---
+++ callmd.py
@@ -0,0 +1,7 @@
+#import md
+from md import fact
+
+name = "kumar"
+print(fact(5))
+# print(name1)
+# print(md.add2(3,4))
---
+++ md.py
@@ -0,0 +1,7 @@
+#  factorial of a number
+def fact(n):
+    if n == 0:
+        return 1
+    return n * fact(n-1)
+
+print(fact(5)) 
